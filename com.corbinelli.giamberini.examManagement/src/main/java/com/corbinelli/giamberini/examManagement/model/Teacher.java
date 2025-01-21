35b60e402b48fb1c8297b5c4de85ad173aff22cc
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2025-01-21 15:53:55 +0100

    Implemented Teacher class

---
+++ com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/model/Teacher.java
@@ -0,0 +1,61 @@
+package com.corbinelli.giamberini.examManagement.model;
+
+import java.util.Objects;
+
+public class Teacher {
+	
+	private Long id;
+	private String name;
+	private String surname;
+	private String email;
+
+	public Teacher(String name, String surname, String email) {
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, name, surname);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Teacher other = (Teacher) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(surname, other.surname);
+	}
+
+	@Override
+	public String toString() {
+		return "Teacher [id=" + id + ", name=" + name + ", surname=" + surname + ", email=" + email + "]";
+	}
+
+}
