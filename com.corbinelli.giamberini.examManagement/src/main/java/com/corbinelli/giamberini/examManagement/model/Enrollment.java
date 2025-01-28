114e034dce0af690c888ef0e9a858e9dd66438c7
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2025-01-28 15:58:40 +0100

    Added JPA annotations to the Enrollment class

--- com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/model/Enrollment.java
+++ com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/model/Enrollment.java
@@ -2,14 +2,36 @@ package com.corbinelli.giamberini.examManagement.model;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "enrollments")
 public class Enrollment {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@OneToOne
 	private Student student;
+	@OneToOne
 	private Exam exam;
 
 	public Enrollment(Student student, Exam exam) {
 		this.student = student;
 		this.exam = exam;
+	}
+	
+	void setId(Long id) {
+		this.id = id;
+	}
+	
+	public Long getId() {
+		return id;
 	}
 
 	public Student getStudent() {
