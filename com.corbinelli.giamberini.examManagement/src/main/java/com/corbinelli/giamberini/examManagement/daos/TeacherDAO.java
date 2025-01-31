66c5629f136d24c6ab898ded6de9ff4917edf9d2
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2025-01-31 11:48:43 +0100

    Implemented TeacherDAO class

---
+++ com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/daos/TeacherDAO.java
@@ -0,0 +1,39 @@
+package com.corbinelli.giamberini.examManagement.daos;
+
+import java.util.List;
+
+import com.corbinelli.giamberini.examManagement.model.Teacher;
+
+import jakarta.persistence.EntityManager;
+
+public class TeacherDAO extends BaseDAO<Teacher>{
+
+	protected TeacherDAO(EntityManager entityManager) {
+		super(entityManager);
+	}
+
+	@Override
+	public Teacher findById(Long id) {
+		return entityManager.find(Teacher.class, id);
+	}
+
+	@Override
+	public List<Teacher> findAll() {
+		return entityManager.createQuery("SELECT t FROM Teacher t", Teacher.class).getResultList();
+	}
+
+	@Override
+	public void save(Teacher teacher) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(teacher);
+		entityManager.getTransaction().commit();
+	}
+
+	@Override
+	public void delete(Teacher teacher) {
+		entityManager.getTransaction().begin();
+		entityManager.remove(teacher);
+		entityManager.getTransaction().commit();
+	}
+
+}
