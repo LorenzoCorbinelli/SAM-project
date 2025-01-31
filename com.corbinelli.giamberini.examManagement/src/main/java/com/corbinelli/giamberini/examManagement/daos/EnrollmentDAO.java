0819e2af8afa90dd34596a3f8d88a6465d92f63d
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-01-31 14:06:21 +0100

    Implemented EnrollmentDAO class

---
+++ com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/daos/EnrollmentDAO.java
@@ -0,0 +1,62 @@
+package com.corbinelli.giamberini.examManagement.daos;
+
+import java.util.List;
+
+import com.corbinelli.giamberini.examManagement.model.Course;
+import com.corbinelli.giamberini.examManagement.model.Enrollment;
+import com.corbinelli.giamberini.examManagement.model.Exam;
+import com.corbinelli.giamberini.examManagement.model.Student;
+
+import jakarta.persistence.EntityManager;
+
+public class EnrollmentDAO extends BaseDAO<Enrollment> {
+
+	public EnrollmentDAO(EntityManager entityManager) {
+		super(entityManager);
+	}
+
+	@Override
+	public Enrollment findById(Long id) {
+		return entityManager.find(Enrollment.class, id);
+	}
+
+	@Override
+	public List<Enrollment> findAll() {
+		return entityManager.createQuery("SELECT e FROM Enrollment e", Enrollment.class).getResultList();
+	}
+
+	@Override
+	public void save(Enrollment enrollment) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(enrollment);
+		entityManager.getTransaction().commit();
+	}
+
+	@Override
+	public void delete(Enrollment enrollment) {
+		entityManager.getTransaction().begin();
+		entityManager.remove(enrollment);
+		entityManager.getTransaction().commit();
+	}
+	
+	public List<Exam> findExamsByStudent(Student student){
+		return entityManager.createQuery("SELECT e.exam FROM Enrollment e WHERE e.student = :student",Exam.class)
+				.setParameter("student", student)
+				.getResultList();
+	}
+	
+	public List<Student> findStudentsByExam(Exam exam){
+		return entityManager.createQuery("SELECT e.student FROM Enrollment e WHERE e.exam = :exam", Student.class)
+				.setParameter("exam", exam)
+				.getResultList();
+	}
+	
+	public List<Exam> findExamsEnrollmentsByStudentAndCourse(Student student, Course course) {
+		return entityManager.createQuery("SELECT e.exam FROM Enrollment e WHERE e.student = :student AND e.exam.course = :course", Exam.class)
+				.setParameter("student", student)
+				.setParameter("course", course)
+				.getResultList();
+	}
+
+	
+}
