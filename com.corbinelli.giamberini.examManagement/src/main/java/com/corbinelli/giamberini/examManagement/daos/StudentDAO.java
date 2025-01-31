9c2747dedacc1679656e1966a576d3587cb06477
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2025-01-31 11:34:17 +0100

    Implemented StudentDAO

---
+++ com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/daos/StudentDAO.java
@@ -0,0 +1,39 @@
+package com.corbinelli.giamberini.examManagement.daos;
+
+import java.util.List;
+
+import com.corbinelli.giamberini.examManagement.model.Student;
+
+import jakarta.persistence.EntityManager;
+
+public class StudentDAO extends BaseDAO<Student> {
+
+	public StudentDAO(EntityManager entityManager) {
+		super(entityManager);
+	}
+
+	@Override
+	public Student findById(Long id) {
+		return entityManager.find(Student.class, id);
+	}
+
+	@Override
+	public List<Student> findAll() {
+		return entityManager.createQuery("SELECT s FROM Student s", Student.class).getResultList();
+	}
+
+	@Override
+	public void save(Student student) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(student);
+		entityManager.getTransaction().commit();
+	}
+
+	@Override
+	public void delete(Student student) {
+		entityManager.getTransaction().begin();
+		entityManager.remove(student);
+		entityManager.getTransaction().commit();
+	}
+
+}
