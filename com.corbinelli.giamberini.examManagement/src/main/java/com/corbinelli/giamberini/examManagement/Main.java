a88f701426edb03e5d248b345f735c2d8e6cf160
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2025-01-31 12:31:08 +0100

    Verified the correct implementation of the DAOs

--- com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/Main.java
+++ com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/Main.java
@@ -1,18 +1,18 @@
 package com.corbinelli.giamberini.examManagement;
 
-import java.time.LocalDate;
-import java.util.List;
 
+import java.time.LocalDate;
+
+import com.corbinelli.giamberini.examManagement.daos.CourseDAO;
+import com.corbinelli.giamberini.examManagement.daos.ExamDAO;
+import com.corbinelli.giamberini.examManagement.daos.TeacherDAO;
 import com.corbinelli.giamberini.examManagement.model.Course;
-import com.corbinelli.giamberini.examManagement.model.Enrollment;
 import com.corbinelli.giamberini.examManagement.model.Exam;
-import com.corbinelli.giamberini.examManagement.model.Student;
 import com.corbinelli.giamberini.examManagement.model.Teacher;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.TypedQuery;
 
 public class Main {
 
@@ -20,64 +20,39 @@ public class Main {
 		// Create an EntityManagerFactory for the persistence unit
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-hibernate-mysql");
 		EntityManager em = emf.createEntityManager();
-
-		em.getTransaction().begin();
-		Student student = new Student("John", "Doe", "john.doe@example.com");
-		em.persist(student);
 		
-		Student student2 = new Student("Sara", "Bianchi", "sara.bianchi@example.com");
-		em.persist(student2);
-
-		Teacher teacher = new Teacher("Mario", "Rossi", "mario.rossi@example.com");
-		em.persist(teacher);
-
+		Teacher teacher1 = new Teacher("Mario", "Rossi", "mario.rossi@example.com");
 		Teacher teacher2 = new Teacher("Luigi", "Verdi", "luigi.verdi@example.com");
-		em.persist(teacher2);
+		
+		Course course1 = new Course("PT", "Penetration Testing", teacher1);
+		Course course2 = new Course("SAM", "SAM", teacher2);
+		Course course3 = new Course("AST", "AST", teacher2);
+		
+		TeacherDAO teacherDAO = new TeacherDAO(em);
+		teacherDAO.save(teacher1);
+		teacherDAO.save(teacher2);
+		
+		CourseDAO courseDAO = new CourseDAO(em);
+		courseDAO.save(course1);
+		courseDAO.save(course2);
+		courseDAO.save(course3);
+		
+		Exam exam1 = new Exam(course1, LocalDate.parse("2025-01-10"));
+		Exam exam2 = new Exam(course1, LocalDate.parse("2025-01-31"));
+		Exam exam3 = new Exam(course2, LocalDate.parse("2025-01-31"));
+		Exam exam4 = new Exam(course3, LocalDate.parse("2025-02-05"));
+		
+		ExamDAO examDAO = new ExamDAO(em);
+		examDAO.save(exam1);
+		examDAO.save(exam2);
+		examDAO.save(exam3);
+		examDAO.save(exam4);
+		
+		System.out.println(examDAO.findAll());
+		System.out.println(examDAO.findExamCoursesByDate(LocalDate.parse("2025-01-31")));
+		System.out.println(examDAO.findExamDatesByCourse(course1));
+		System.out.println(examDAO.findExamsInAPeriod(LocalDate.parse("2025-01-15"), LocalDate.parse("2025-02-15")));
 
-		Course course = new Course("SAM", "Software Architectures and Methodologies", teacher);
-		em.persist(course);
-		
-		Course course2 = new Course("PT", "Penetration Testing", teacher2);
-		em.persist(course2);
-
-		Exam exam = new Exam(course, LocalDate.parse("2025-01-12"));
-		em.persist(exam);
-		
-		Exam exam2 = new Exam(course2, LocalDate.parse("2025-01-23"));
-		em.persist(exam2);
-		
-		Enrollment enrollment = new Enrollment(student, exam);
-		em.persist(enrollment);
-		
-		Enrollment enrollment2 = new Enrollment(student2, exam2);
-		em.persist(enrollment2);
-		
-		Enrollment enrollment3 = new Enrollment(student, exam2);
-		em.persist(enrollment3);
-		em.getTransaction().commit();
-		
-		TypedQuery<Student> allStudents = em.createQuery("Select s from Student s", Student.class);
-		List<Student> resultList = allStudents.getResultList();
-		System.out.println("List of students: " + resultList);
-		
-		TypedQuery<Teacher> teacherQuery = em.createQuery("Select t from Teacher t where surname =: teacherSurname", 
-				Teacher.class);
-		teacherQuery.setParameter("teacherSurname", "Verdi");
-		Teacher result = teacherQuery.getSingleResult();
-		System.out.println("Teacher Verdi: " + result);
-		
-		TypedQuery<Student> examStudents = em.createQuery(
-				"Select en.student from Enrollment en JOIN en.exam ex JOIN ex.course c where c.name =: courseName", 
-				Student.class);
-		examStudents.setParameter("courseName", "SAM");
-		resultList = examStudents.getResultList();
-		System.out.println("Students registered at the SAM exam: " + resultList);
-		
-		
-		examStudents.setParameter("courseName", "PT");
-		resultList = examStudents.getResultList();
-		System.out.println("Students registered at the PT exam: " + resultList);
-		
 		// Close the EntityManager
 		em.close();
 		emf.close();
