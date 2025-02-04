355a7376fea08877ffb3652e83d694529642da02
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2025-02-04 15:06:43 +0100

    Validation of StudentService and TeacherService

--- com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/Main.java
+++ com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/Main.java
@@ -1,17 +1,12 @@
 package com.corbinelli.giamberini.examManagement;
 
 
-import java.time.LocalDate;
 
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
-import com.corbinelli.giamberini.examManagement.model.Course;
-import com.corbinelli.giamberini.examManagement.model.Exam;
 import com.corbinelli.giamberini.examManagement.model.Student;
 import com.corbinelli.giamberini.examManagement.model.Teacher;
-import com.corbinelli.giamberini.examManagement.services.CourseService;
-import com.corbinelli.giamberini.examManagement.services.ExamService;
 import com.corbinelli.giamberini.examManagement.services.StudentService;
 import com.corbinelli.giamberini.examManagement.services.TeacherService;
 
@@ -27,28 +22,29 @@ public class Main {
 		StudentService studentService = container.select(StudentService.class).get();
 		
 		Student biagio = studentService.registerStudent("Biagio", "Antonacci");
-		studentService.registerStudent("Sara", "Cartocci");
-		studentService.registerStudent("Matteo", "Bianchi"); 
-		studentService.registerStudent("Veronica", "Derilli");
+		Student sara = studentService.registerStudent("Sara", "Antonacci");
+		Student biagio2 = studentService.registerStudent("Biagio", "Antonacci");
+		Student biagio3 = studentService.registerStudent("Biagio", "Antonacci");
+		Student biagio4 = studentService.registerStudent("Biagio", "Rossi");
+		Student veronica = studentService.registerStudent("Veronica", "Derilli");
 		
-		System.out.print(biagio.toString());
+		System.out.println(biagio);
+		System.out.println(sara);
+		System.out.println(biagio2);
+		System.out.println(biagio3);
+		System.out.println(biagio4);
+		System.out.println(veronica);
 		
 		TeacherService teacherService = container.select(TeacherService.class).get();
 		Teacher teacher = teacherService.registerTeacher("Mario", "Rossi");
+		Teacher teacher2 = teacherService.registerTeacher("Mario", "Rossi");
+		Teacher teacher3 = teacherService.registerTeacher("Luigi", "Rossi");
+		Teacher teacher4 = teacherService.registerTeacher("Mario", "Verdi");
 		
-		CourseService courseService = container.select(CourseService.class).get();
-		
-		Course PT = courseService.insertCourse("PT", "Penetration Testing", teacher);
-		Course AST = courseService.insertCourse("AST", "Automated Software Testing", teacher);
-		
-		System.out.println(courseService.getAllCourses());
-		System.out.println(courseService.getCoursesByPartialDescription("testing"));
-		System.out.println(courseService.getCoursesByName("AST"));
-		
-		ExamService examService = container.select(ExamService.class).get();
-		Exam exam = new Exam(PT, LocalDate.parse("2025-01-12"));
-		examService.insertExam(exam);
-		System.out.println(examService.getExamCoursesInDate("2025-01-12"));
+		System.out.println(teacher);
+		System.out.println(teacher2);
+		System.out.println(teacher3);
+		System.out.println(teacher4);
 		
 		weld.shutdown();
 	}
