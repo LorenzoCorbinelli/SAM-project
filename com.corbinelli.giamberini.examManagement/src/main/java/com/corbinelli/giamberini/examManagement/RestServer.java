471488fdef861e4f95abdbb53de5f63e9673e880
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2025-03-05 12:41:21 +0100

    Attempt to introduce JAX-RS

---
+++ com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/RestServer.java
@@ -0,0 +1,25 @@
+package com.corbinelli.giamberini.examManagement;
+
+import java.net.URI;
+
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class RestServer {
+
+	private static final String BASE_URI = "http://localhost:8080/";
+
+	public static void main(String[] args) {
+		System.out.println("Starting Grizzly HTTP server...");
+
+		// Register your JAX-RS resources
+		final ResourceConfig rc = new ResourceConfig().packages("com.corbinelli.giamberini.examManagement.resources");
+
+		// Start the server
+		GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+
+		System.out.println("JAX-RS app running at: " + BASE_URI);
+		System.out.println("Press Ctrl + C to stop...");
+	}
+
+}
