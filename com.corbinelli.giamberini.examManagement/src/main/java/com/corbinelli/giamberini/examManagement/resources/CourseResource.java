d2f86f995b780d2819c8074083ed599638eab721
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-03-06 13:13:24 +0100

    Attempt to manage CourseResource APIs

---
+++ com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/resources/CourseResource.java
@@ -0,0 +1,72 @@
+package com.corbinelli.giamberini.examManagement.resources;
+
+import com.corbinelli.giamberini.examManagement.model.Course;
+import com.corbinelli.giamberini.examManagement.services.CourseService;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/courses")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CourseResource {
+
+	@Inject
+	private CourseService courseService;
+
+	@GET
+	@Path("/{id}")
+	public Response getCourse(@PathParam("id") Long id) {
+		Course course = courseService.getCourseInfoById(id);
+		if (course == null) {
+			return Response.status(Response.Status.NOT_FOUND)
+					.build();
+		}
+		return Response.status(Response.Status.OK)
+				.entity(course)
+				.build();
+	}
+	
+	/*@GET
+	@Path("/{name}")
+	public Response getCourse(@PathParam("name") String name) {
+		List<Course> coursesByName = courseService.getCoursesByName(name);
+		if (coursesByName.isEmpty()) {
+			return Response.status(Response.Status.NOT_FOUND)
+					.build();
+		}
+		return Response.status(Response.Status.OK)
+				.entity(coursesByName)
+				.build();
+	}*/
+	
+	@POST
+	public Response newCourse(Course course) {
+		System.out.println("Received Course: " + course);
+		courseService.insertCourse(course);
+		return Response.status(Response.Status.CREATED)
+				.entity(course)
+				.build();
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public Response deleteCourse(@PathParam("id") Long id) {
+		Course course = courseService.removeCourse(id);
+		if (course == null) {
+			return Response.status(Response.Status.NOT_FOUND)
+					.build();
+		}
+		return Response.status(Response.Status.CREATED)
+				.entity(course)
+				.build();
+	}
+}
