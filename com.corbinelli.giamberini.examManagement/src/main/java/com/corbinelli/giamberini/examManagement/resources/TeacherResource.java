15fa4aa3c256c46b86785021da9f30845271ff09
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-03-06 11:52:09 +0100

    TeacherResource APIs

---
+++ com.corbinelli.giamberini.examManagement/src/main/java/com/corbinelli/giamberini/examManagement/resources/TeacherResource.java
@@ -0,0 +1,60 @@
+package com.corbinelli.giamberini.examManagement.resources;
+
+import org.hibernate.annotations.Parameter;
+
+import com.corbinelli.giamberini.examManagement.model.Teacher;
+import com.corbinelli.giamberini.examManagement.services.TeacherService;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/teachers")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class TeacherResource {
+
+	@Inject
+	private TeacherService teacherService;
+
+	@GET
+	@Path("/{id}")
+	public Response getTeacher(@PathParam("id") Long id) {
+		Teacher teacher = teacherService.getTeacherInfoById(id);
+		if (teacher == null) {
+			return Response.status(Response.Status.NOT_FOUND)
+					.build();
+		}
+		return Response.status(Response.Status.OK)
+				.entity(teacher)
+				.build();
+	}
+	
+	@POST
+	public Response newTeacher(Teacher teacher) {
+		teacherService.registerTeacher(teacher);
+		return Response.status(Response.Status.CREATED)
+				.entity(teacher)
+				.build();
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public Response deleteTeacher(@PathParam("id") Long id) {
+		Teacher teacher = teacherService.deleteTeacher(id);
+		if (teacher == null) {
+			return Response.status(Response.Status.NOT_FOUND)
+					.build();
+		}
+		return Response.status(Response.Status.OK)
+				.entity(teacher)
+				.build();
+	}
+}
